e7984c1cd81143a16c5c1115a3e1ff85a302a03c
Salyonaya <salyonaya@gmail.com>
2020-12-05 03:51:10 +0200

    Initial commit

---
+++ src/Name.kt
@@ -0,0 +1,24 @@
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
+fun main() {
+   //println("some value: %.2f - ok, %f, %d, 0x%H, '%s'".format(55.123456, 33.012, 44, 44, "uyx"))
+    val GOAL = 200
+    val DAYS = 31
+    val DAYGOAL = GOAL / DAYS
+    val STARTDAY = "2020-11-30"
+    val goal  = ChronoUnit.DAYS.between(LocalDate.parse(STARTDAY), LocalDateTime.now()) * DAYGOAL
+    val passed = readLine()!!.toFloat()
+
+    val goodass = arrayOf("Now or never", "Never look back", "Live without regrets", "Never say never", "Keep Strong", "You can do better")
+    val badass = arrayOf("You are the best!", "Keep it up!", "Great job!", "Excellent result!")
+
+    println("Now you've gone ${"%.2f".format(passed / GOAL * 100)}% of your way\n" +
+            "You need ${"%.2f".format(goal - passed)}h more\n")
+
+    println(if (passed > goal) "${badass[(Math.random() * badass.size).toInt()]}" else "${goodass[(Math.random() * goodass.size).toInt()]}")
+}
+
+
+
