7bef7883d51fc59a223338ee341c43c5c007698a
Salyonaya <salyonaya@gmail.com>
2020-12-05 11:19:14 +0200

    small changes

--- src/Name.kt
+++ src/Name.kt
@@ -10,13 +10,13 @@ fun main() {
     val goal  = ChronoUnit.DAYS.between(LocalDate.parse(STARTDAY), LocalDateTime.now()) * DAYGOAL
     val passed = readLine()!!.toFloat()
 
-    val goodass = arrayOf("Now or never", "Never look back", "Live without regrets", "Never say never", "Keep Strong", "You can do better")
-    val badass = arrayOf("You are the best!", "Keep it up!", "Great job!", "Excellent result!")
+    val badass = arrayOf("Now or never", "Never look back", "Live without regrets", "Never say never", "Keep Strong", "You can do better")
+    val goodass = arrayOf("You are the best!", "Keep it up!", "Great job!", "Excellent result!")
 
     println("Now you've gone ${"%.2f".format(passed / GOAL * 100)}% of your way\n" +
             "You need ${"%.2f".format(goal - passed)}h more\n")
 
-    println(if (passed > goal) "${badass[(Math.random() * badass.size).toInt()]}" else "${goodass[(Math.random() * goodass.size).toInt()]}")
+    println(if (passed < goal) "${badass[(Math.random() * badass.size).toInt()]}" else "${goodass[(Math.random() * goodass.size).toInt()]}")
 }
 
 
