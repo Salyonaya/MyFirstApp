34d8b8f0f146e6ae2ac5e0185464f323784ba483
Salyonaya <salyonaya@gmail.com>
2020-12-07 00:15:14 +0200

    Add functions

--- src/Name.kt
+++ src/Name.kt
@@ -1,6 +1,10 @@
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
+import kotlin.math.floor
+
+fun hours(h: Double) = h.toInt()
+fun minutes(h: Double) = ((h - floor(h)) * 60).toInt()
 
 fun main() {
     val GOAL = 200
@@ -8,16 +12,16 @@ fun main() {
     val DAYGOAL = GOAL / DAYS
     val STARTDAY = "2020-11-30"
     val goal  = ChronoUnit.DAYS.between(LocalDate.parse(STARTDAY), LocalDateTime.now()) * DAYGOAL
-    val passed = readLine()!!.toFloat()
+    val passedhours = readLine()!!.toDouble()
+    val passedminutes = readLine()!!.toDouble()
+    val passed = passedhours + (passedminutes/60)
+    val h = (goal - passed)
+
 
     val badass = arrayOf("Now or never", "Never look back", "Live without regrets", "Never say never", "Keep Strong", "You can do better")
     val goodass = arrayOf("You are the best!", "Keep it up!", "Great job!", "Excellent result!")
 
     println("Now you've gone ${"%.2f".format(passed / GOAL * 100)}% of your way\n" +
-            "You need ${"%.2f".format(goal - passed)}h more\n")
-
-    println(if (passed < goal) "${badass[(Math.random() * badass.size).toInt()]}" else "${goodass[(Math.random() * goodass.size).toInt()]}")
+            "You should spend for Kotlin $goal hours")
+    println("You need ${hours(h)}h ${minutes(h)}m more - " + if (passed < goal) badass[(Math.random() * badass.size).toInt()] else goodass[(Math.random() * goodass.size).toInt()])
 }
-
-
-
